970b6af3b5ad709e8a19673ae0e492a7488f9431
Mikael Meira <mikaelmenardi@gmail.com>
2023-04-17 12:22:27 -0300

    Exercicio Finalizado

--- alunos.js
+++ alunos.js
@@ -1,17 +1,57 @@
 const alunos = [
-    { nome: 'Pedro', matricula: '0001', media: 8.5 },
-    { nome: 'João', matricula: '0002', media: 7.2 },
-    { nome: 'Ana', matricula: '0003', media: 9.0 },
-    { nome: 'Maria', matricula: '0004', media: 6.5 },
-  ];
-  
-  function filtrarPorNome(nome) {
-    return alunos.filter(aluno => aluno.nome.toLowerCase().includes(nome.toLowerCase()));
-  }
-  
-  function filtrarPorMedia(mediaMinima) {
-    return alunos.filter(aluno => aluno.media >= mediaMinima);
-  }
-  
-  module.exports = { alunos, filtrarPorNome, filtrarPorMedia };
-  
+    { nome: "Mikael", media: 8.5 },
+    { nome: "Meira", media: 8 },
+    { nome: "Almir", media: 7 },
+    { nome: "Gabriel", media: 6 }
+];
+
+
+function listaAlunos(nome, media) {
+    let listaFiltro = alunos;
+    if (nome) {
+        listaFiltro = listaFiltro.filter(s => s.nome.toLowerCase().includes(nome.toLowerCase()));
+    }
+    else if (media) {
+        listaFiltro = listaFiltro.filter(s => s.media >= media);
+    }
+    return listaFiltro;
+}
+
+function deleteAlunos(index) {
+    const alunosRemovido = alunos.splice(index, 1);
+    return alunosRemovido.length > 0 ? alunosRemovido[0] : null;
+}
+
+const fs = require('fs');
+const path = require('path');
+
+function updateFile(index, nome, media) {
+    if (index >= 0 && index < alunos.length) {
+        const updateFile = alunos[index];
+        updateFile.nome = nome || updateFile.nome;
+        updateFile.media = media || updateFile.media;
+
+        //Desafio 0:
+        fs.writeFile(
+            path.join(__dirname, 'db.json'),
+            JSON.stringify(alunos),
+            err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Dados atualizados!');
+                }
+            }
+        );
+
+        return updateFile;
+    } else {
+        return null
+    }
+};
+
+
+
+
+
+module.exports = { alunos, listaAlunos, deleteAlunos, updateFile};
--- index.js
+++ index.js
@@ -1,35 +1,77 @@
-const express = require('express');
-const { alunos, filtrarPorNome, filtrarPorMedia } = require('./alunos');
+const express = require("express");
+const alunos = require("./alunos");
 
 const app = express();
 
-app.get('/alunos', (req, res) => {
-  let resultado = alunos;
-
-  if (req.query.nome) {
-    resultado = filtrarPorNome(req.query.nome);
-  }
-
-  if (req.query.media) {
-    resultado = filtrarPorMedia(req.query.media);
-  }
-
-  res.json(resultado);
-});
-
+const morgan = require('morgan');
 app.use(express.json());
 
-app.post('/alunos/novo', (req, res) => {
-  const { nome, matricula, media } = req.body;
-
-  if (!nome || !matricula || !media) {
-    return res.status(400).json({ mensagem: 'Dados inválidos' });
-  }
-
-  const novoAluno = { nome, matricula, media };
-  alunos.push(novoAluno);
-
-  res.status(201).json(novoAluno);
+app.get("/alunos", (req, res) => {
+    const { nome, media } = req.query;
+    const list = alunos.listaAlunos(nome, media);
+    res.json(list);
 });
 
-app.listen(3000, () => console.log('Servidor iniciado na porta 3000'));
+
+app.post("/alunos/novo", (req, res) => {
+    const { nome, media } = req.body;
+    if (!nome || !media) {
+        res.status(400).json({ error: "Informações incompletas" });
+    } else {
+        const novoAluno = { nome: nome, media: media };
+        alunos.alunos.push(novoAluno);
+        res.status(201).json({ message: `Usuário ${nome} adicionado com sucesso` });
+    }
+});
+
+
+app.post("/alunos/delete/:index", (req, res) => {
+    const index = req.params.index;
+    const deleteAlunos = alunos.deleteAlunos(index);
+
+    if (deleteAlunos) {
+        res.json(deleteAlunos);
+    } else {
+        res.status(404).json({ mensagem: "Aluno não encontrado no sistema" });
+    }
+});
+
+
+app.post("/alunos/atualizar/:index", (req, res) => {
+    const index = parseInt(req.params.index);
+    const { nome, media } = req.body;
+    const updateFile = alunos.updateFile(index, nome, media);
+
+    if (updateFile) {
+        res.json(updateFile);
+    } else {
+        res.status(404).send('Aluno não encontrado no sistema!');
+    }
+});
+
+app.put("/alunos/:index", morgan('tiny'), (req, res) => {
+    const index = Number(req.params.index);
+    const { nome, media } = req.body;
+    const updateFile = alunos.updateFile(index, nome, media);
+
+    if(updateFile) {
+        res.json(updateFile);
+    } else {
+        res.status(404).json({mensagem: "Aluno não encontrado no sistema"})
+    }
+});
+
+app.delete("/alunos/:index", morgan('tiny'), (req, res) => {
+    const index = Number(req.params.index);
+    const deleteAlunos = alunos.deleteAlunos(index);
+
+    if(deleteAlunos) {
+        res.json(deleteAlunos);
+    } else {
+        res.status(404).json({mensagem: "Aluno não encontrado no sistema"})
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000/")
+});
