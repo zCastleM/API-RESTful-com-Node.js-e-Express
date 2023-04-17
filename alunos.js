452e9739e9515951ce7d9f62d75e915908c60798
mikaelmeira <116334877+zCastleM@users.noreply.github.com>
2023-04-17 16:23:14 -0300

    Update alunos.js

--- alunos.js
+++ alunos.js
@@ -8,6 +8,31 @@ const alunos = [
 const fs = require('fs');
 const path = require('path');
 
+// updateFile: recebe três parâmetros: "index", "nome" e "media". Atualiza os dados do aluno na posição "index" da lista de alunos com os valores fornecidos para "nome" e/ou "media". Em seguida, grava os dados atualizados em um arquivo JSON usando o módulo "fs" e o caminho relativo "./db.json". Se o índice fornecido for inválido, retorna null.
+function updateFile(index, nome, media) {
+    if (index >= 0 && index < alunos.length) {
+        const updateFile = alunos[index];
+        updateFile.nome = nome || updateFile.nome;
+        updateFile.media = media || updateFile.media;
+
+        fs.writeFile(
+            path.join(__dirname, 'db.json'),
+            JSON.stringify(alunos),
+            err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Dados atualizados com sucesso!');
+                }
+            }
+        );
+
+        return updateFile;
+    } else {
+        return null
+    }
+};
+
 // listaAlunos: recebe dois parâmetros opcionais, "nome" e "media", e retorna uma lista de alunos que correspondem aos critérios de filtro. Se "nome" for fornecido, a função filtra a lista para incluir apenas alunos cujos nomes incluam o valor de "nome" (ignorando maiúsculas e minúsculas). Se "media" for fornecida, a função filtra a lista para incluir apenas alunos cujas médias sejam maiores ou iguais à "media" fornecida.
 function listaAlunos(nome, media) {
     let listaFiltro = alunos;
@@ -24,32 +49,6 @@ function deleteAlunos(index) {
     const alunosRemovido = alunos.splice(index, 1);
     return alunosRemovido.length > 0 ? alunosRemovido[0] : null;
 }
-// updateFile: recebe três parâmetros: "index", "nome" e "media". Atualiza os dados do aluno na posição "index" da lista de alunos com os valores fornecidos para "nome" e/ou "media". Em seguida, grava os dados atualizados em um arquivo JSON usando o módulo "fs" e o caminho relativo "./db.json". Se o índice fornecido for inválido, retorna null.
-function updateFile(index, nome, media) {
-    if (index >= 0 && index < alunos.length) {
-        const updateFile = alunos[index];
-        updateFile.nome = nome || updateFile.nome;
-        updateFile.media = media || updateFile.media;
-
-        fs.writeFile(
-            path.join(__dirname, 'db.json'),
-            JSON.stringify(alunos),
-            err => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    console.log('Dados atualizados!');
-                }
-            }
-        );
-
-        return updateFile;
-    } else {
-        return null
-    }
-};
-
-
 
 
 
