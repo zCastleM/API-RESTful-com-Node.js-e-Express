84c61e5180aba5ec99f6bbbf92f7c38206efc902
mikaelmeira <116334877+zCastleM@users.noreply.github.com>
2023-04-17 16:22:31 -0300

    Update alunos.js

--- alunos.js
+++ alunos.js
@@ -5,7 +5,10 @@ const alunos = [
     { nome: "Gabriel", media: 6 }
 ];
 
+const fs = require('fs');
+const path = require('path');
 
+// listaAlunos: recebe dois parâmetros opcionais, "nome" e "media", e retorna uma lista de alunos que correspondem aos critérios de filtro. Se "nome" for fornecido, a função filtra a lista para incluir apenas alunos cujos nomes incluam o valor de "nome" (ignorando maiúsculas e minúsculas). Se "media" for fornecida, a função filtra a lista para incluir apenas alunos cujas médias sejam maiores ou iguais à "media" fornecida.
 function listaAlunos(nome, media) {
     let listaFiltro = alunos;
     if (nome) {
@@ -16,22 +19,18 @@ function listaAlunos(nome, media) {
     }
     return listaFiltro;
 }
-
+// deleteAlunos: recebe um parâmetro "index" que representa a posição do aluno na lista de alunos a ser removido e retorna o objeto de aluno removido. Se o índice fornecido for inválido, retorna null.
 function deleteAlunos(index) {
     const alunosRemovido = alunos.splice(index, 1);
     return alunosRemovido.length > 0 ? alunosRemovido[0] : null;
 }
-
-const fs = require('fs');
-const path = require('path');
-
+// updateFile: recebe três parâmetros: "index", "nome" e "media". Atualiza os dados do aluno na posição "index" da lista de alunos com os valores fornecidos para "nome" e/ou "media". Em seguida, grava os dados atualizados em um arquivo JSON usando o módulo "fs" e o caminho relativo "./db.json". Se o índice fornecido for inválido, retorna null.
 function updateFile(index, nome, media) {
     if (index >= 0 && index < alunos.length) {
         const updateFile = alunos[index];
         updateFile.nome = nome || updateFile.nome;
         updateFile.media = media || updateFile.media;
 
-        //Desafio 0:
         fs.writeFile(
             path.join(__dirname, 'db.json'),
             JSON.stringify(alunos),
